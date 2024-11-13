5ad76addd54e74280876456e08064d17c8afbe38
neeraj0304 <neerajsahu6644@gmail.com>
2024-11-13 17:08:17 +0530

    fix errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Newbutton from '../components/Newbutton'
+import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className=' flex h-[79vh] justify-center items-center text-center text-white text-m md:text-3xl'>
@@ -7,7 +8,7 @@ function Home() {
         <h1 className=' p-1 font-chivo'>WELCOME TO</h1>
         <h1 className=' p-1 font-special text-4xl text-cyan-500'>PROTYPER</h1>
         <h1 className='  pb-2 font-chivo'>LEARN TO SPEED UP YOUR PRODUCTIVITY<span className='text-cyan-500 font-bolder text-4xl'>!</span></h1>
-        <Newbutton name={"practice"}/>
+        <Link to={"/practice"}><Newbutton name={"practice"}/></Link>
       </div>
     </div>
   )
